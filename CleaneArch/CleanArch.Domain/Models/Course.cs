89cb8c82c55809b1221ae3b742ae9b3885db1985
tzafi <tzafip@gmail.com>
2021-01-28 23:24:28 +0200

    Add Course Domain Modle

---
+++ CleaneArch/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleaneArch.Domain.Models
+{
+    public class Course
+    {
+         public int ID { get; set; }
+
+        public string Name { get; set; }
+        public string  Description { get; set; }
+        public string ImgeURL { get; set; }
+    }
+}
