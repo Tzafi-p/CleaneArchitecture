98e648fc096a9e2e41d82490bd0133fe28b0fd07
tzafi <tzafip@gmail.com>
2021-01-31 12:59:15 +0200

    Initial Commit of  Infrastructure.Data Project, Update MVC for new UniversityDBContext

--- CleaneArch/CleanArch.MVC/Startup.cs
+++ CleaneArch/CleanArch.MVC/Startup.cs
@@ -1,4 +1,5 @@
 using CleanArch.MVC.Data;
+using CleaneArch.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -34,6 +35,10 @@ namespace CleanArch.MVC
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("UniversityDBConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ CleaneArch/CleaneArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CleaneArch.Domain.Models;
+
+namespace CleaneArch.Infra.Data.Context
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set;}
+    }
+}
